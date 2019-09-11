b10e93aa0920f3288c2921a700bad1fa0e703b03
Karl Hobley <karl@kaed.uk>
2019-09-11 17:12:42 +0100

    Switch to standard rq

--- wagtail_localize_pontoon/views.py
+++ wagtail_localize_pontoon/views.py
@@ -1,14 +1,17 @@
-import django_rq
+from rq import Queue
+from worker import conn
 
 from django.shortcuts import render, redirect
 
+from worker import conn
 from .models import PontoonSyncLog, PontoonResource
 from .sync import sync, sync_running
 
 
-def dashboard(request):
-    queue = django_rq.get_queue('wagtail_localize_pontoon.sync')
+queue = Queue('wagtail_localize_pontoon.sync', connection=conn)
 
+
+def dashboard(request):
     return render(request, 'wagtail_localize_pontoon/dashboard.html', {
         'resources': PontoonResource.objects.all(),
         'logs': PontoonSyncLog.objects.order_by('-time'),
@@ -20,7 +23,6 @@ def dashboard(request):
 def force_sync(request):
     if not sync_running():
         # Make sure there is only one job in the queue at a time
-        queue = django_rq.get_queue('wagtail_localize_pontoon.sync')
         queue.delete()
         queue.enqueue(sync)
 
