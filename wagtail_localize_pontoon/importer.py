eb4e574a718d732cea403ee5fe2443ebb61776ba
Karl Hobley <karl@kaed.uk>
2019-10-08 14:27:21 +0100

    Merge create_or_update_translated_page into Importer

--- wagtail_localize_pontoon/importer.py
+++ wagtail_localize_pontoon/importer.py
@@ -33,15 +33,60 @@ class Importer:
 
         Note, all strings in the submission must be translated into the target language!
         """
-        revision, created = create_or_update_translated_page(
-            submission.revision, language
+        locale = Locale.objects.get(
+            region_id=Region.objects.default_id(), language=language
         )
+
+        page = submission.revision.as_page_object()
+
+        try:
+            translated_page = page.get_translation(locale)
+            created = False
+        except page.specific_class.DoesNotExist:
+            # May raise ParentNotTranslatedError
+            translated_page = page.copy_for_translation(locale)
+            created = True
+
+        # Fetch all translated segments
+        segment_page_locations = SegmentPageLocation.objects.filter(
+            page_revision=submission.revision
+        ).annotate_translation(language)
+
+        template_page_locations = TemplatePageLocation.objects.filter(
+            page_revision=submission.revision
+        ).select_related("template")
+
+        segments = []
+
+        for page_location in segment_page_locations:
+            segment = SegmentValue(page_location.path, page_location.translation)
+            segments.append(segment)
+
+        for page_location in template_page_locations:
+            template = page_location.template
+            segment = TemplateValue(
+                page_location.path,
+                template.template_format,
+                template.template,
+                template.segment_count,
+            )
+            segments.append(segment)
+
+        # Ingest all translated segments into page
+        ingest_segments(page, translated_page, page.locale.language, language, segments)
+
+        # Make sure the slug is valid
+        translated_page.slug = slugify(translated_page.slug)
+        translated_page.save()
+
+        new_revision = translated_page.save_revision()
+        new_revision.publish()
 
         PontoonResourceTranslation.objects.create(
-            submission=submission, language=language, revision=revision
+            submission=submission, language=language, revision=new_revision
         )
 
-        return revision, created
+        return new_revision, created
 
     def import_resource(self, resource, language, old_po, new_po):
         for changed_entry in set(new_po) - set(old_po):
@@ -117,56 +162,3 @@ class Importer:
 
             # Check if the translated page is ready to be created/updated
             self.try_update_resource_translation(resource, language)
-
-
-def create_or_update_translated_page(revision, language):
-    locale = Locale.objects.get(
-        region_id=Region.objects.default_id(), language=language
-    )
-
-    page = revision.as_page_object()
-
-    try:
-        translated_page = page.get_translation(locale)
-        created = False
-    except page.specific_class.DoesNotExist:
-        # May raise ParentNotTranslatedError
-        translated_page = page.copy_for_translation(locale)
-        created = True
-
-    # Fetch all translated segments
-    segment_page_locations = SegmentPageLocation.objects.filter(
-        page_revision=revision
-    ).annotate_translation(language)
-
-    template_page_locations = TemplatePageLocation.objects.filter(
-        page_revision=revision
-    ).select_related("template")
-
-    segments = []
-
-    for page_location in segment_page_locations:
-        segment = SegmentValue(page_location.path, page_location.translation)
-        segments.append(segment)
-
-    for page_location in template_page_locations:
-        template = page_location.template
-        segment = TemplateValue(
-            page_location.path,
-            template.template_format,
-            template.template,
-            template.segment_count,
-        )
-        segments.append(segment)
-
-    # Ingest all translated segments into page
-    ingest_segments(page, translated_page, page.locale.language, language, segments)
-
-    # Make sure the slug is valid
-    translated_page.slug = slugify(translated_page.slug)
-    translated_page.save()
-
-    new_revision = translated_page.save_revision()
-    new_revision.publish()
-
-    return new_revision, created
