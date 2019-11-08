c8e44acaf739b5c4067c2e47b7351bac5e8043e3
Karl Hobley <karl@kaed.uk>
2019-11-08 17:14:07 +0000

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 setup(
     name="wagtail-localize-pontoon",
-    version="0.1.1",
+    version="0.1.2",
     description="Pontoon integration for Wagtail Localize",
     author="Karl Hobley",
     author_email="karl@torchbox.com",
