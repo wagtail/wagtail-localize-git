91b0ec99dc275fca70b8e7eaba93725d722ad2cb
Karl Hobley <karl@kaed.uk>
2020-11-17 12:50:15 +0000

    Add wagtail-localize to requirements

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
     install_requires=[
         "Django>=2.2,<3.2",
         "Wagtail>=2.11,<2.12",
+        "wagtail-localize>=0.9,<=1.1",
         "pygit2>=1.0,<2.0",
         "gitpython>=3.0,<4.0",
         "toml>=0.10,<0.11"
