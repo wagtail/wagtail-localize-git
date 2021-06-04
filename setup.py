1b03288a389d9b09b080138b799297f7d59d2693
Karl Hobley <karl@torchbox.com>
2021-06-04 16:31:44 +0100

    Test against Wagtail 2.12 and 2.13

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     ],
     install_requires=[
         "Django>=2.2,<3.2",
-        "Wagtail>=2.11,<2.12",
+        "Wagtail>=2.11,<2.14",
         "wagtail-localize>=0.9,<=1.1",
         "pygit2>=1.0,<2.0",
         "gitpython>=3.0,<4.0",
